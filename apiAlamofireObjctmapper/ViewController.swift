6a4d8b0fd8ca4230a22bd9e6b98ad9113aa733c9
nirmal <nirmal251992@gmail.com>
2021-02-15 12:01:09 +0530

    Initial Commit

---
+++ apiAlamofireObjctmapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apiAlamofireObjctmapper
+//
+//  Created by Nirmal on 15/02/21.
+//  Copyright © 2021 Nirmal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
